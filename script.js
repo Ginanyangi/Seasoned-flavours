df5ae733c6a9c325310b709b4efb32b29d2c0b61
Ginanyangi <georginakabasa2018@gmail.com>
2024-03-26 11:24:32 +0300

    add new html style and js files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
-let url = "www.themealdb.com/api/json/v1/1/search.php?s";
+let url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}';
 
 searchBtn.addEventListener("click", () => {
     let userInp = document.getElementById("user-inp").value;
